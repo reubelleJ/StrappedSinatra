9aec0660b74856ee940dfadaa64576fbe0104ea0
Reubelle Junio <reubellejunio@Users-MacBook-Pro-60.local>
2017-10-30 02:23:40 -0500

    Bootstrapped sinatra files

---
+++ web.rb
@@ -0,0 +1,51 @@
+require 'sinatra'
+require 'data_mapper' # metagem, requires common plugins too.
+
+# need install dm-sqlite-adapter
+# if on heroku, use Postgres database
+# if not use sqlite3 database I gave you
+if ENV['DATABASE_URL']
+  DataMapper::setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+else
+  DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/blog.db")
+end
+
+class Post
+    include DataMapper::Resource
+    property :id, Serial
+    property :title, String
+    property :body, Text
+    property :created_at, DateTime
+end
+
+# Perform basic sanity checks and initialize all relationships
+# Call this when you've defined all your models
+DataMapper.finalize
+
+# automatically create the post table
+Post.auto_upgrade!
+
+get '/new' do
+	erb :new
+end
+
+post '/create' do
+	#replace this
+    blog = Post.new
+    blog.title = params[:Title]
+    blog.body = params[:Body]
+    blog.save
+	return "Posted Successfully!"
+end
+
+get '/' do
+	#load all posts
+	#display them
+	@title = "All posts"
+	@headline = "My life in blog"
+	@paragraph = "Follow me plz. Follow 4 Follow."
+	@posts = Post.all
+
+
+	erb :index
+end
